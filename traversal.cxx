b2462704a0167e0d69456b0ad1ed0c3de9e55bbb
arthurkindo <arthurkindo0@gmail.com>
2020-11-17 10:23:59 +0530

    traversal 0.1

---
+++ traversal.cxx
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter the size of the array : ";
+	cin>>n;
+	int arr[n];
+	for(int i=0; i<n; i++){
+		cin>>arr[i];
+		arr[i] *= 2;
+	}
+	for(int i=0; i<n; i++){
+		cout<<arr[i]<<" ";
+	}
+	return 0;
+}
+
+
+
+
+
+
+
