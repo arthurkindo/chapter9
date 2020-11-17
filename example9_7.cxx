2272e5c03ed5dd824801877353d37e995b0a8a61
arthurkindo <arthurkindo0@gmail.com>
2020-11-17 13:17:30 +0530

    set version 0.3

--- example9_7.cxx
+++ example9_7.cxx
@@ -10,11 +10,21 @@ int main()
 		// taking the size and element 
 	cout<<"Enter the size of the array : ";
 	cin>>size;
-	
+	cout<<"Enter the elements : "<<endl;
+	for(int i=0; i<size; i++){
+		cin>>arr[i];
+	}
+	cout<<"Enter the item : ";
+	cin>>item;
+	ptr = set_(arr, size, item);
+		// displaying the array
+	for(int i=0; i<size; i++){
+		cout<<*(ptr+i)<<" ";
+	}
 	return 0;
 }
 // creating set function
-int * set_(int arr[], int size; int item){
+int * set_(int arr[], int size, int item){
 	int n=0;
 	for(int i=0; i<size; i++){
 		if(arr[i] == item){
